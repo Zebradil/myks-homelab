de3c405b7ae9ab2e534582860aebddad242b93c1
German Lashevich <german.lashevich@gmail.com>
2023-12-03 01:37:05 +0100

    chore: myks init + minor base configuration

---
+++ hack/git/git-diff-ignore-versions.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This script is used to ignore version changes in git diff.
+# It replaces values of specific fields in the given file with a placeholder.
+# Add more fields to the list if needed.
+
+yq '(.metadata.labels | select(has("app.kubernetes.io/version"))                           | .["app.kubernetes.io/version"]) = "dummy" |
+    (.metadata.labels | select(has("helm.sh/chart"))                                       | .["helm.sh/chart"])             = "dummy" |
+    (.spec.template.metadata.labels | select(has("app.kubernetes.io/version"))             | .["app.kubernetes.io/version"]) = "dummy" |
+    (.spec.template.metadata.labels | select(has("chart"))                                 | .["chart"])                     = "dummy" |
+    (.spec.template.metadata.labels | select(has("helm.sh/chart"))                         | .["helm.sh/chart"])             = "dummy" |
+    (.metadata.labels | select(has("chart"))                                               | .["chart"])                     = "dummy"' \
+    "$1"
