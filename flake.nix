17b42d7c6719a93ac843e8161fcf38b5ab8e4b1b
German Lashevich <german.lashevich@gmail.com>
2025-12-10 11:02:30 +0100

    chore(flake): remove flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -3,28 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { ... }@inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            mise
-          ];
-          shellHook = ''
-            mise install
-            source <(mise activate)
-          '';
-        };
-      }
-    );
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              mise
+            ];
+            shellHook = ''
+              mise install
+              source <(mise activate)
+            '';
+          };
+        }
+      );
+    };
 }
