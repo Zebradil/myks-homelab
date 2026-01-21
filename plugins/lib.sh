b9e8901898ee4ed8cc5d1d29f61ee123d7a6fa30
German Lashevich <german.lashevich@gmail.com>
2026-01-21 13:03:40 +0100

    chore: ensure ARGOCD_SERVER is set in argo-refresh plugin

--- plugins/lib.sh
+++ plugins/lib.sh
@@ -4,5 +4,15 @@
 function l:argocd() {
   argocd \
     --grpc-web \
+    --server "${ARGOCD_SERVER:?}" \
     "$@"
+}
+
+# l:ensure_login ensures that the user is logged in to Argo CD
+function l:ensure_login() {
+  status="$(l:argocd account get-user-info --output yaml | yq .loggedIn)"
+  if [[ "$status" != "true" ]]; then
+    echo "Logging in to Argo CD..."
+    l:argocd login "${ARGOCD_SERVER:?}"
+  fi
 }
