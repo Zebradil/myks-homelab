24b2623be509cf7b92420038d1854e1913918621
German Lashevich <german.lashevich@gmail.com>
2026-06-08 18:21:33 +0200

    fix(lib): update plugins

--- plugins/lib.sh
+++ plugins/lib.sh
@@ -10,8 +10,9 @@ function l:argocd() {
 
 # l:ensure_login ensures that the user is logged in to Argo CD
 function l:ensure_login() {
-  status="$(l:argocd account get-user-info --output yaml | yq .loggedIn)"
-  if [[ "$status" != "true" ]]; then
+  local logged_in
+  logged_in="$(l:argocd account get-user-info --output yaml 2>/dev/null | yq .loggedIn || true)"
+  if [[ "$logged_in" != "true" ]]; then
     echo "Logging in to Argo CD..."
     l:argocd login "${ARGOCD_SERVER:?}"
   fi
